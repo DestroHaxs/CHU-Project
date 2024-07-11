ffb73ac3baf778c76a4009e20013647017c745da
Ahmed <ayman.39.ab@gmail.com>
2024-07-11 19:25:36 +0100

    homepage correction

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import { FaFacebookF, FaInstagram, FaLinkedinIn, FaYoutube, FaPlus } from 'react-icons/fa';
+import Navbar from './Navbar'; 
+import Socials from './Socials'; 
 
-function Socials() {
+function HomePage() {
   return (
-    <div className="fixed left-0 top-2/3 transform -translate-y-1/2 flex flex-col items-center space-y-4 opacity-70">
-      <a href="#" className="transition duration-300 ease-in-out text-blue-500 hover:text-blue-700">
-        <FaFacebookF size={14} />
-      </a>
-      <a href="#" className="transition duration-300 ease-in-out text-blue-500 hover:text-blue-700">
-        <FaInstagram size={14} />
-      </a>
-      <a href="#" className="transition duration-300 ease-in-out text-blue-500 hover:text-blue-700">
-        <FaLinkedinIn size={14} />
-      </a>
-      <a href="#" className="transition duration-300 ease-in-out text-blue-500 hover:text-blue-700">
-        <FaYoutube size={14} />
-      </a>
-      <div className="w-px h-12 bg-blue-500"></div>
-      <a href="#" className="relative flex flex-col items-center justify-center bg-blue-500 text-white w-16 h-48 hover:bg-blue-700 transition duration-300 ease-in-out fixed bottom-0 left-0 mb-0">
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -rotate-90 flex items-center">
-          <span className="whitespace-nowrap text-xs font-bold">
-            ACCÈS DIRECTS
-          </span>
-          <FaPlus className="ml-2 text-lg font-bold" />
-        </div>
-      </a>
+    <div className="min-h-screen bg-white">
+      <Navbar /> 
+      <Socials /> 
     </div>
   );
 }
 
-export default Socials;
+export default HomePage;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css'; // Import Tailwind CSS
 import HomePage from './HomePage';
 import Navbar from './Navbar';
+import Socials from './Socials';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HomePage /> 
     {/* <Navbar/> */}
+    {/* <Socials/> */}
   </React.StrictMode>
 );
